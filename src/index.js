13348ee1aa43b7211c1f7a7f05f76642830dc82e
victor-myha <victormyhaletsky@gmail.com>
2023-02-07 16:44:47 +0200

    settings before deploy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
@@ -8,10 +8,10 @@ import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/goit-react-hw-08-phonebook'>
       <Provider store={store}>
-        <App/>
+        <App />
       </Provider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
