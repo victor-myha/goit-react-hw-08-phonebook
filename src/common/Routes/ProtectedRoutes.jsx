0df99cdfc9f06d2bfabbf257368fc6566f85d10e
victor-myha <victormyhaletsky@gmail.com>
2023-02-07 23:47:11 +0200

    fix routes

--- src/common/Routes/ProtectedRoutes.jsx
+++ src/common/Routes/ProtectedRoutes.jsx
@@ -1,41 +1,22 @@
-import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
-import Contacts from '../../components/Contacts/Contacts';
+import PropTypes from 'prop-types';
+import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import Login from '../../components/Login/Login';
-import Register from '../../components/Register/Register';
-import { Error404 } from '../../components/Error404/Error404';
-import { useEffect } from 'react';
+import { useMemo } from 'react';
 
-const ProtectedRoutes = () => {
-  const navigate = useNavigate();
+const ProtectedRoutes = ({ isSecured = false, element }) => {
   const token = useSelector((state) => state.userSlice.user?.token);
 
-  useEffect(() => {
-    navigate('/');
-  }, [token, navigate]);
+  const isAuthorized = useMemo(() => {
+    return Boolean(token);
+  }, [token]);
 
-  return (
-    <>
-      <Routes>
-        {
-          token ? (
-            <>
-              <Route path={'/'} element={<Navigate to={'/contacts'} />} />
-              <Route path={'/contacts'} element={<Contacts />} />
-              <Route path='*' element={<Error404 />} />
-            </>
-          ) : (
-            <>
-              <Route path={'/'} element={<Navigate to={'/login'} />} />
-              <Route path={'/login'} element={<Login />} />
-              <Route path={'/register'} element={<Register />} />
-              <Route path='*' element={<Error404 />} />
-            </>
-          )
-        }
-      </Routes>
-    </>
-  );
+  if (isSecured && !isAuthorized) return <Navigate to={'/login'} />;
+  if (!isSecured && isAuthorized) return <Navigate to={'/contacts'} />;
+  return element;
 };
 
+ProtectedRoutes.propTypes = {
+  isSecured: PropTypes.bool,
+  element: PropTypes.element.isRequired,
+};
 export default ProtectedRoutes;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,11 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { setUser } from '../redux/userSlice';
 import ProtectedRoutes from '../common/Routes/ProtectedRoutes';
+import { Route, Routes } from 'react-router-dom';
+import { Error404 } from './Error404/Error404';
+import Contacts from './Contacts/Contacts';
+import Login from './Login/Login';
+import Register from './Register/Register';
 
 export const App = () => {
   const dispatch = useDispatch();
@@ -18,7 +23,12 @@ export const App = () => {
       <div>
         <Navigation />
       </div>
-      <ProtectedRoutes />
+      <Routes>
+        <Route path={'/login'} element={<ProtectedRoutes element={<Login />} />} />
+        <Route path={'/register'} element={<ProtectedRoutes element={<Register />} />} />
+        <Route path={'/contacts'} element={<ProtectedRoutes isSecured element={<Contacts />} />} />
+        <Route path={'*'} element={<Error404 />} />
+      </Routes>
     </div>
   );
 };
